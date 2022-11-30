4170ab126ac24892adc2f39ad30e633edb6b4ea8
ellajang <3lla.jang@gmail.com>
2022-12-01 02:31:52 +0900

    fourth commit

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,29 @@ function App() {
   useEffect(()=>{
     authService.onAuthStateChanged((user) => {
       if(user){
-        setIsLoggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName:user.displayName,
+          uid:user.uid,
+          updateProfile:(args) => user.updateProfile(args),
+        });
       }else{
         setIsLoggedIn(false);
+        setUserObj(null);
       }
         setInit(true);
     });
   },[]);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName:user.displayName,
+      uid:user.uid,
+      updateProfile:(args) => user.updateProfile(args),
+    });
+  }
   return(
     <>
-    {init ? <AppRouter isLoggedIn = {isLoggedIn} userObj={userObj} /> : "Initializing..."}
+    {init ? <AppRouter refreshUser={refreshUser} isLoggedIn = {Boolean(userObj)} userObj={userObj} /> : "Initializing..."}
   </>
   );
 }
